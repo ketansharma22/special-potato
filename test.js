91de723b8101c84f98d00e8cdd6f48ab045ec485
ketansharma22 <kanu220504@gmail.com>
2024-07-22 21:43:40 +0530

    more

--- test.js
+++ test.js
@@ -55,3 +55,31 @@ let n=5
 
 //~~~~ Write a Program to print the Full Pyramid Star pattern.
 
+// ~~~ program to print hour clock pattern
+
+
+// downside pyramid
+// for (let i = 0; i < n ; i++) {
+//   // printing spaces
+//   for (let j = 0; j < i; j++) {
+//      process.stdout.write(' ');
+//   }
+//   // printing star
+//   for (let k = 0; k < (n - i) * 2 - 1; k++) {
+//     process.stdout.write('*');
+//   }
+//   console.log();
+// }
+
+// // Upside pyramid
+// for (let i = 2; i <= n; i++) {
+//   // printing spaces
+//   for (let j = 1; j <= n - i; j++) {
+//     process.stdout.write(' ')
+//   }
+//   // printing star
+//   for (let k = 0; k < 2 * i - 1; k++) {
+//     process.stdout.write('*')
+//   }
+//   console.log();
+// }
