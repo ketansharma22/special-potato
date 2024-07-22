6d044fdadc20836e5cdc6cb362be189b2dcedaaa
ketansharma22 <kanu220504@gmail.com>
2024-07-22 20:17:33 +0530

    4 pattern problems solved

---
+++ test.js
@@ -0,0 +1,57 @@
+let n=5
+// ~~~~ Write a Program to print the right triangle star pattern.
+// for(let i=0;i<5;i++){
+//     let star=""
+//     for(let j=0;j<=i;j++)
+//     {
+//         star+="*"
+
+//     }
+//     console.log(star)
+// }
+
+//~~~~ Write a Program to print an inverted right triangle star pattern
+
+// for(let i=0;i<5;i++){
+//     let star=""
+//     for(let j=5;j>i;j--){
+//         star+="*"
+//     }
+//     console.log(star);
+// }
+
+//~~~~ Write a Program to print mirrored right triangle star pattern.
+
+
+// for(let i=1;i<=n;i++){
+//     let star=""
+//     //n-i spaces in every row as i is ith row 
+//     for( let j=1;j<=n;j++){
+//         if(j<=(n-i)){
+//             star+=" "
+//             continue
+//         }   
+//         star+="*"
+//     }
+//     console.log(star);
+// }
+
+
+// ~~~~ Write a Program to print the Inverted Half Pyramid Star Pattern.
+
+// for(let i=0;i<n;i++){
+//     let star=""
+//     for(let j=0;j<n;j++){
+//         if(j<i){
+//             star+=" "
+//             continue
+//         }
+
+//         star+="*"
+//     }
+//     console.log(star);
+// }
+
+
+//~~~~ Write a Program to print the Full Pyramid Star pattern.
+
